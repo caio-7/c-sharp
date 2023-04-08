9d8b5df34d07c1ce4a348849c719eca9aaea1539
Caio <caio.s.ferreira@hotmail.com>
2023-04-08 00:51:33 -0300

    Adicionado função divisão e loop do programa

--- 02_Calculadora/Program.cs
+++ 02_Calculadora/Program.cs
@@ -1,6 +1,13 @@
-﻿Console.WriteLine("Bem-Vindo a calculadora!");
+﻿
+
+Console.WriteLine("Bem-Vindo a calculadora!");
 Console.WriteLine("Escolha a operação a ser realizada:\n");
+
+//Após a primeira utilização, mantém programa em loop para outras operações
+while(true){
+
 Console.WriteLine("1-Soma\n2-Subtração\n3-Multiplicação\n4-Divisão\n0-Sair\n");
+
 
 //Leitura da opção escolhida
 int opcao = Convert.ToInt32(Console.ReadLine());
@@ -18,7 +25,8 @@ switch(opcao)
         n1 = Convert.ToDecimal(Console.ReadLine());
         Console.WriteLine("Digite o segundo valor:");
         n2 = Convert.ToDecimal(Console.ReadLine());
-        Console.WriteLine($"\n{n1} + {n2} = {n1+n2}");
+        Console.WriteLine("\nResultado:");
+        Console.WriteLine($"{n1} + {n2} = {n1+n2}\n");
         
         break;
 
@@ -27,7 +35,7 @@ switch(opcao)
         n1 = Convert.ToDecimal(Console.ReadLine());
         Console.WriteLine("Digite o segundo valor:");
         n2 = Convert.ToDecimal(Console.ReadLine());
-        Console.WriteLine($"\n{n1} - {n2} = {n1-n2}");
+        Console.WriteLine($"\n{n1} - {n2} = {n1-n2}\n");
         break;
 
     case 3:
@@ -35,7 +43,20 @@ switch(opcao)
         n1 = Convert.ToDecimal(Console.ReadLine());
         Console.WriteLine("Digite o segundo valor:");
         n2 = Convert.ToDecimal(Console.ReadLine());
-        Console.WriteLine($"\n{n1} * {n2} = {n1*n2}");
+        Console.WriteLine($"\n{n1} * {n2} = {n1*n2}\n");
+        break;
+
+    case 4:
+        Console.WriteLine("Digite o primeiro valor:");
+        n1 = Convert.ToDecimal(Console.ReadLine());
+        Console.WriteLine("Digite o segundo valor:");
+        n2 = Convert.ToDecimal(Console.ReadLine());
+        Console.WriteLine($"\n{n1} / {n2} = {n1/n2}\n");
+        break;
+    
+    default:
+        Console.WriteLine("\nOpção inválida\n");
         break;
 }
 
+}
